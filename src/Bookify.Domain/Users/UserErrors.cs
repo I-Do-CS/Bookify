d187bbdb2dc5f5aaeea31e74e26fa37b8263ae6e
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-08-27 20:48:12 +0330

    add domain layer

---
+++ src/Bookify.Domain/Users/UserErrors.cs
@@ -0,0 +1,16 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+
+public static class UserErrors
+{
+    public static readonly Error NotFound = new(
+        "User.NotFound",
+        "The User with the specified identifier was not found"
+    );
+
+    public static readonly Error InvalidCredentials = new(
+        "User.InvalidCredentials",
+        "The provided credentials were invalid"
+    );
+}
