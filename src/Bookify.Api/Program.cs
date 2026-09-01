f76509445ed58ecc00a21509fb8f76514fe2572f
Ali Ghelichkhani <idocs4aliving@gmail.com>
2026-09-01 17:08:15 +0330

    add structured logging with serilog

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,8 +1,13 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(
+    (context, configuration) => configuration.ReadFrom.Configuration(context.Configuration)
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -24,6 +29,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRequestContextLogging();
+
+app.UseSerilogRequestLogging();
 
 app.UseCustomExceptionHandling();
 
